2a261f317d753c026bc4c482924bcac6890ff25a
Lukas Prochazka <prochazka.lu@gmail.com>
2024-10-28 15:39:27 +0100

    Revert

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,5 @@
-import { join } from 'node:path';
-import { createRequestHandler } from '@expo/server/adapter/vercel';
+const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
-const buildPath =
-  typeof __dirname !== 'undefined'
-    ? join(__dirname, '../dist/server')
-    : join(new URL('.', import.meta.url).pathname, '../dist/server');
-
-export default createRequestHandler({
-  build: buildPath,
+module.exports = createRequestHandler({
+  build: require('path').join(__dirname, '../dist/server'),
 });
